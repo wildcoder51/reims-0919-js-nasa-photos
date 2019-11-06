9bd438673954c4fcbf82e4f00ecf7cab87102a7c
Jessy Pinchon <jessy.pinchon@gmail.com>
2019-11-06 16:20:56 +0100

    before pull vers. API called picture day

--- picspace/src/Components/RandomPictures.js
+++ picspace/src/Components/RandomPictures.js
@@ -1,21 +1,56 @@
 import React from "react";
+import axios from 'axios';
 import './RandomPictures.css';
 
-const RandomPicture = ({randomPictures}) => {
-  return (
-    <article className="RandomPictures">
-      {
-        randomPictures.map(
-          picture => (
-            <figure className='box-random-pictures'>
-              <img className='img-random-pictures' src ={picture.hdurl} alt="y'en a pas t'as vu"/>
-            </figure>
-          )
-        )
-      }
-    </article>
-      
-  )
-}
+class RandomPictures extends React.Component {
+  constructor(props){
+    super(props)
+    this.state = {
+      name :"",
+      url:"",
+      arrayPictures: [],
+    }
+  this.getRandomPictures = this.getRandomPictures.bind(this);
+  }
 
-export default RandomPicture;
+  getRandomPictures() {
+    // Send the request
+    axios.get('http://hubblesite.org/api/v3/images/all')
+      // Extract the DATA from the received response
+      .then(response => response.data)
+      // Use this data to update the state
+      .then(data => {console.log(data);
+      });
+  
+    };
+  
+
+  componentDidMount() {
+    this.getRandomPictures()
+  }
+
+  // listOfPictures = (picture) =>{
+  //   return (
+  //     <RandomPictures name={picture.name} url={picture.url}/>
+  //   )
+  // }
+
+  
+
+  render() {
+
+    return (
+      <article className="RandomPictures">
+        {
+        this.state.arrayPictures.map(picture => 
+        (<figure className='box-random-pictures'>
+        <img className='img-random-pictures' src ={picture.url} alt={picture.name}/>
+        </figure>
+            )
+          )
+        }
+      </article>
+    );
+  }
+}
+export default RandomPictures;
