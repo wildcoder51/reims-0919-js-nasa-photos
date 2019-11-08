c6665396ab341963d1db9440551d0801048bea1d
Maxime Féry <maxime.fery@outlook.fr>
2019-11-08 06:42:11 +0100

    add button selection

---
+++ picspace/src/Components/Navbar.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import './Navbar.css';
+
+
+
+function Navbar() {
+  return(
+    <div className='Navbar'>
+      <p><span className="titre">PicSpace</span></p>
+      <button className="panier">Ma selection</button>
+    </div>
+  );
+}
+
+
+
+export default Navbar;
