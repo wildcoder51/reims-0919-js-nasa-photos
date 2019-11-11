d5e34008b1d5156952851a35f98fa2f6219052a5
Sophie <strochain@gmail.com>
2019-11-11 20:47:18 +0100

    Logo NASA added close to the application name

--- picspace/src/Components/Header.js
+++ picspace/src/Components/Header.js
@@ -6,7 +6,10 @@ import './Header.css';
 const Header = () => {
   return (
     <div className="Header">
+      <div className="titleLogo">
       <div className="titre">PicSpace</div>
+      <img src="https://www.nasa.gov/sites/all/themes/custom/nasatwo/images/nasa-logo.svg"/>
+      </div>
         <div className="HeaderLinks">
           <div className="Headerlink"><Link className="link" to="/">Home</Link></div>
           <div className="Headerlink"><Link className="link" to="/Basket">My selection</Link></div>
