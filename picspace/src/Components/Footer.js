0eb67b4f3d2b4dd38310420305c51ffcb32c1b55
Maxime Féry <maxime.fery@outlook.fr>
2019-11-13 11:54:38 +0100

    css on header and basket

--- picspace/src/Components/Footer.js
+++ picspace/src/Components/Footer.js
@@ -9,40 +9,40 @@ const Footer = () => {
 
       <div className="footer">
         <div >
-        <p>Website created by :</p>
+          <p>Website created by :</p>
         </div>
 
-          <div className="listProfile">
+        <div className="listProfile">
 
           <div className="personnalProfile">
-          <p>Sylvie :</p>
-          <div className="logos">
-          <a href="https://github.com/R1SsophieFEHU"><img className="logoGithub" src="https://upload.wikimedia.org/wikipedia/commons/thumb/9/91/Octicons-mark-github.svg/1200px-Octicons-mark-github.svg.png" alt="Sylvie"/></a>
-          <a href="https://github.com/R1SsophieFEHU"><img className="logoLinkedin" src="https://culliganrecrute.fr/wp-content/uploads/2018/01/linkedin-icon.png" alt="Sylvie"/></a>
-          </div>
+            <p>Sylvie :</p>
+            <div className="logos">
+              <a href="https://github.com/R1SsophieFEHU"><img className="logoGithub" src="https://upload.wikimedia.org/wikipedia/commons/thumb/9/91/Octicons-mark-github.svg/1200px-Octicons-mark-github.svg.png" alt="Sylvie"/></a>
+              <a href="https://github.com/R1SsophieFEHU"><img className="logoLinkedin" src="https://culliganrecrute.fr/wp-content/uploads/2018/01/linkedin-icon.png" alt="Sylvie"/></a>
+            </div>
 
-          </div>
-
-          <div className="personnalProfile">
-          <p>Maxime :</p>
-          <a href="https://github.com/R1SsophieFEHU"><img className="logoGithub" src="https://upload.wikimedia.org/wikipedia/commons/thumb/9/91/Octicons-mark-github.svg/1200px-Octicons-mark-github.svg.png" alt="Maxime"/></a>
-          <a href="https://github.com/R1SsophieFEHU"><img className="logoLinkedin" src="https://culliganrecrute.fr/wp-content/uploads/2018/01/linkedin-icon.png" alt="Maxime"/></a>
           </div>
 
           <div className="personnalProfile">
-          <p>Jessy :</p>
-          <a href="https://github.com/R1SsophieFEHU"><img className="logoGithub" src="https://upload.wikimedia.org/wikipedia/commons/thumb/9/91/Octicons-mark-github.svg/1200px-Octicons-mark-github.svg.png" alt="Jessy"/></a>
-          <a href="https://github.com/R1SsophieFEHU"><img className="logoLinkedin" src="https://culliganrecrute.fr/wp-content/uploads/2018/01/linkedin-icon.png" alt="Jessy"/></a>
+            <p>Maxime Féry:</p>
+            <a href="https://github.com/Max2810"><img className="logoGithub" src="https://upload.wikimedia.org/wikipedia/commons/thumb/9/91/Octicons-mark-github.svg/1200px-Octicons-mark-github.svg.png" alt="Maxime"/></a>
+            <a href="https://www.linkedin.com/in/maxime-f%C3%A9ry-450769192/"><img className="logoLinkedin" src="https://culliganrecrute.fr/wp-content/uploads/2018/01/linkedin-icon.png" alt="Maxime"/></a>
           </div>
 
           <div className="personnalProfile">
-          <p>Sophie :</p>
-          <a href="https://github.com/R1SsophieFEHU"><img className="logoGithub" src="https://upload.wikimedia.org/wikipedia/commons/thumb/9/91/Octicons-mark-github.svg/1200px-Octicons-mark-github.svg.png" alt="Sophie"/></a>
-          <a href="https://github.com/R1SsophieFEHU"><img className="logoLinkedin" src="https://culliganrecrute.fr/wp-content/uploads/2018/01/linkedin-icon.png" alt="Sophie"/></a>
-          </div>
+            <p>Jessy Pinchon:</p>
+            <a href="https://github.com/R1SsophieFEHU"><img className="logoGithub" src="https://upload.wikimedia.org/wikipedia/commons/thumb/9/91/Octicons-mark-github.svg/1200px-Octicons-mark-github.svg.png" alt="Jessy"/></a>
+            <a href="https://github.com/R1SsophieFEHU"><img className="logoLinkedin" src="https://culliganrecrute.fr/wp-content/uploads/2018/01/linkedin-icon.png" alt="Jessy"/></a>
           </div>
 
+          <div className="personnalProfile">
+            <p>Sophie Trochain:</p>
+            <a href="https://github.com/R1SsophieFEHU"><img className="logoGithub" src="https://upload.wikimedia.org/wikipedia/commons/thumb/9/91/Octicons-mark-github.svg/1200px-Octicons-mark-github.svg.png" alt="Sophie"/></a>
+            <a href="https://github.com/R1SsophieFEHU"><img className="logoLinkedin" src="https://culliganrecrute.fr/wp-content/uploads/2018/01/linkedin-icon.png" alt="Sophie"/></a>
+          </div>
         </div>
+
+      </div>
 
     </div>
 
