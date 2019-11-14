39d1b03917bbc368a12ba8797e5367402e2b1e53
Sophie <strochain@gmail.com>
2019-11-14 11:34:41 +0100

    add print, mail and download button + CSS

--- picspace/src/Components/Basket.js
+++ picspace/src/Components/Basket.js
@@ -4,10 +4,17 @@ import './Basket.css';
 function Basket({content}){
   return(
     <div>
-      {content && <ul>
+      <div>
+      {content && <figure className="basketBlock">
         {content.map((picture) => 
-        <img src={picture} alt=""/>)}
-      </ul>}
+        <img className="basketPicture" src={picture} alt=""/>)}
+      </figure>}
+      </div >
+      <div className="listButton">
+      <img className="buttonPrint" src="http://stagewp.sharethis.com/wp-content/uploads/2017/05/Print.png" alt=""/>
+      <img className="buttonDownload" src="https://img.vola.fr/d/icons/VOLA-WEB-PICTO-TELECHARGER-BLANC.png" alt=""/>
+      <img className="buttonMail" src="https://png.pngtree.com/png-clipart/20190520/original/pngtree-email-symbol-icon-png-image_3569787.jpg" alt=""/>
+      </div>
     </div>
   );
 }
