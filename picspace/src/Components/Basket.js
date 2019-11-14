bf7a63d3194a2ae0924527795e7f3fb0a1b33fdc
Sophie <strochain@gmail.com>
2019-11-14 13:11:17 +0100

    style button added

--- picspace/src/Components/Basket.js
+++ picspace/src/Components/Basket.js
@@ -11,9 +11,9 @@ function Basket({content}){
       </figure>}
       </div >
       <div className="listButton">
-      <img className="buttonPrint" src="http://stagewp.sharethis.com/wp-content/uploads/2017/05/Print.png" alt=""/>
-      <img className="buttonDownload" src="https://img.vola.fr/d/icons/VOLA-WEB-PICTO-TELECHARGER-BLANC.png" alt=""/>
-      <img className="buttonMail" src="https://png.pngtree.com/png-clipart/20190520/original/pngtree-email-symbol-icon-png-image_3569787.jpg" alt=""/>
+      <button className="buttonClic"><img className="buttonPrint" src="http://stagewp.sharethis.com/wp-content/uploads/2017/05/Print.png" alt=""/></button>
+      <button className="buttonClic"><img className="buttonDownload" src="https://img.vola.fr/d/icons/VOLA-WEB-PICTO-TELECHARGER-BLANC.png" alt=""/></button>
+      <button className="buttonClic"><img className="buttonMail" src="https://png.pngtree.com/png-clipart/20190520/original/pngtree-email-symbol-icon-png-image_3569787.jpg" alt=""/></button>
       </div>
     </div>
   );
