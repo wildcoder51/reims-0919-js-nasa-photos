09a2b66921da57e6b436eb551998ac9f3dd43ec1
Sophie <strochain@gmail.com>
2019-12-05 12:11:32 +0100

    button delete basket add and center in the page

--- picspace/src/Components/Basket.js
+++ picspace/src/Components/Basket.js
@@ -8,10 +8,12 @@ function Basket({content, deleter}){
       <div>
         {content && <figure className="basketBlock">
           {content.map((picture) => 
-            <img className="basketPicture" src={picture} alt=""/>)}
-            <button type="button" onClick= {() =>deleter(content.picture)} >Delete</button> 
-        </figure>}
+            <img className="basketPicture" src={picture} alt=""/>)}             
+        </figure>}         
       </div >
+      <div className="deleteCenter">
+        <button type="button" onClick= {() =>deleter(content.picture)} >Delete basket</button>
+      </div>
       <div className="listButton">
         <button className="buttonClic"><img className="buttonPrint" src="http://stagewp.sharethis.com/wp-content/uploads/2017/05/Print.png" alt=""/></button>
         <button className="buttonClic"><img className="buttonDownload" src="https://img.vola.fr/d/icons/VOLA-WEB-PICTO-TELECHARGER-BLANC.png" alt=""/></button>
